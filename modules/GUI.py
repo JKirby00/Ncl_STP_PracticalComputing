58c8793a047742ae4d9585d572f9677523e301d2
Josh Kirby <Joshua.Kirby@nhs.net>
2026-01-02 17:11:11 +0000

    Added some basic boilerplate to work from

---
+++ modules/GUI.py
@@ -0,0 +1,34 @@
+'''
+Module to sort the graphical user interface (GUI) to use
+in the MiniPACS.
+'''
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+class MainGui(QMainWindow):
+    '''This is the class that defines the main PACS graphical window.'''
+    def __init__(self):
+        '''This function is called when the MainGui class is first
+        initialised'''
+        super(MainGui, self).__init__()
+        uic.loadUi(r"./ui/pacs_home.ui" ,self)# loads the UI from .ui file
+        self.show()
+
+
+def ShowGui():
+    '''
+    Very simple function that shows the main PACS GUI
+    by creating a PyQt application, creates an instance of our
+    UI class and then executes the QApplication.
+
+    Args: Nothing
+    Returns: Nothing
+    '''
+    app = QApplication([])
+    window = MainGui()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    ShowGui()
