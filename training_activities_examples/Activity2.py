d1ccb392099a61984920bbce48652362ac82d708
Liam Walker <liam.walker6@nhs.net>
2026-01-07 15:20:24 +0000

    Created a duplicate folder to store working examples for each activity

---
+++ training_activities_examples/Activity2.py
@@ -0,0 +1,5 @@
+'''File to be used for activity 2. This activity asks
+you to write a function called CalculateAgeFromDob
+that calulates the patient's age given the date of birth
+given to the function as a string.'''
+
