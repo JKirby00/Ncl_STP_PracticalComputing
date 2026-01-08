c5d6103a5cf4ead292ba741864df150a635afce1
Josh Kirby <Joshua.Kirby@nhs.net>
2026-01-08 16:36:25 +0000

    Updated activity 1 and 2 examples

--- training_activities_examples/Session1/Activity1.py
+++ training_activities_examples/Session1/Activity1.py
@@ -2,4 +2,18 @@
 involves writing a function to print the patient
 data to the console.'''
 
+def PrintDataToConsole(patient_details):
+    '''
+    Functon to print out the patient details to the console.
+    
+    Args:
+        patient_details (dict) = A dict of patient details with
+            keys: id, MRN, Name, DateOfBirth and Address
 
+    Returns nothing
+    '''
+    print("_")
+    print("The following patient has been clicked on by the user:")
+    print(f"Name: {patient_details['Name']} ({patient_details['MRN']})")
+    print(f"Date of birth: {patient_details['DateOfBirth']}")
+    print(f"Address: {patient_details['Address']}")
