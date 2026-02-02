757e02f4fcdc53ec5cdc93d6b4edd3614e8792f6
Paola Cognigni <paola.cognigni@nhs.net>
2026-02-02 13:47:01 +0000

    Update tests to match database content

--- training_activities_examples/Session3/ActivityC1.py
+++ training_activities_examples/Session3/ActivityC1.py
@@ -14,12 +14,12 @@ class TestDatabaseQueries(unittest.TestCase):
     def test_get_patients_retrieves_patient_details_from_database(self):
         result = get_patients()
         # if you insert any new patients into the database, this count will fail
-        self.assertEqual(len(result), 5)
+        self.assertEqual(len(result), 7)
         first_result = result[0]
-        self.assertEqual(first_result, ("First", "patient", "details"))
+        self.assertEqual(first_result, (1, "123456B", "John Smith", "22/03/1956", "221B Baker Street, London"))
         # any new patients will be added at the end, so this will also fail
         last_result = result[-1]
-        self.assertEqual(last_result, ("Last", "patient", "details"))
+        self.assertEqual(last_result, (7, "654321", "John Doe", "01/01/1990", "404 Testing St"))
 
     # integration failure
     def test_get_patients_cannot_retrieve_data_from_database_if_not_connected(self):
