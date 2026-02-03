c87c7827da614ac61955c20f5ccfd5271b317b92
Jack Parker <jack.parker2@NHS.net>
2026-02-03 09:59:14 +0000

    Added example data and some extra hints in session 1 activity 1

--- training_activities/Session1/Activity1.py
+++ training_activities/Session1/Activity1.py
@@ -1,5 +1,23 @@
 '''File to be used for activity 1. This activity
 involves writing a function to print the patient
-data to the console.'''
+data to the console. Remember, call the function
+PrintDataToConsole and get it to have a single argument
+(input) called patient_details'''
+
+
+
+if __name__ == "__main__":
+    # This code will be run when you run this file directly
+    # This can be very helpful for testing functions
+
+    patient_dict = {
+        "id":1,
+        "MRN":"123456A",
+        "Name":"Bob Duckworth",
+        "DateOfBirth":"12/06/2001",
+        "Address":"The Barn, Windy Street, Big Wood"
+    }
+
+    PrintDataToConsole(patient_details = patient_dict)
 
 
