4fcb5ee5fcd44984058e3093f72bb6a23d05c27d
John Cope <john.cope2@nhs.net>
2026-02-04 10:18:53 +0000

    Altered MRN to match database

--- training_activities/Session3/Activity1.py
+++ training_activities/Session3/Activity1.py
@@ -1,5 +1,8 @@
 """Activity - Databases
 
+Pre-requisite:
+    Either install SQLite Viewer from VSCode extensions or view the database is SQLite DBBrowser
+
 1) Write some simple SQL queries into the empty dictionary 'sql_queries' below to retrieve the required information (diff=1-2)
 2) Complete the query_database function to connect to the database, execute a query and return the results (diff=1)
 3) Write functions to use your SQL queries and query_database function to return the results (diff=1)
@@ -64,7 +67,7 @@ if __name__ == "__main__":
     print("Task2 query:", query_result)
     
     # Update the following to test your SQL queries requiring parameters
-    query_result = query_database(sql_queries["get_patient_by_mrn"], ("123456",))
+    query_result = query_database(sql_queries["get_patient_by_mrn"], ("123456B",))
     print("Task2 query:", query_result)
     
     # Update the following to test your SQL functions
